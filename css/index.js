65723f7090e10123c2032e9a25dfb0b820e17b42
CodeBreaker231 <66872986+EswarBenarjee@users.noreply.github.com>
2021-12-02 19:43:10 +0530

    Add files via upload

---
+++ css/index.js
@@ -0,0 +1,59 @@
+function validate() {
+    var nv = /[A-Za-z]/;
+    var name = document.getElementById("name").value;
+    var flag= 0 ;
+    if(nv.test(name)) {
+        document.getElementById("name").style.border = 0;
+        document.getElementById("checkname").innerHTML = "";
+    } else {
+        document.getElementById("name").style.border = "2px solid red";
+        document.getElementById("checkname").innerHTML = "<br>Enter only characters for name";
+    }
+    var jntuc = /[0-9A-Za-z]{10}/;
+    var jntu = document.getElementById("jntuno").value;
+    if(jntuc.test(jntu)) {
+        document.getElementById("jntuno").style.border = 0;
+        document.getElementById("checkjntu").innerHTML = "";
+    } else {
+        document.getElementById("jntuno").style.border = "2px solid red";
+        document.getElementById("checkjntu").innerHTML = "<br>Enter JNTU number correctly";
+        flag = 1;
+    }
+    var pv = /[0-9]{10}/;
+    var phno = document.getElementById("phno").value;
+    if(pv.test(phno)) {
+        document.getElementById("phno").style.border = 0;
+        document.getElementById("checknum").innerHTML = "";
+    } else {
+        document.getElementById("phno").style.border = "2px solid red";
+        document.getElementById("checknum").innerHTML = "<br>Enter only numbers for Contact number";
+        flag = 1;
+    }
+    var mem = document.getElementById("cmember").value;
+    if(mem == 'n') {
+        document.getElementById("non-member").style.display = "block";
+        var f = check();
+        if(!f) {
+          document.getElementById("tranid").style.border = "2px solid red";
+          document.getElementById('trancheck').innerHTML = "Please enter Transaction ID";
+          flag = 1;
+        } else {
+          document.getElementById("tranid").style.border = "";
+          document.getElementById('trancheck').innerHTML = "";
+        }
+    } else {
+        document.getElementById("non-member").style.display = "none";
+    }
+
+    if(flag==1) {
+        return false;
+    }
+}
+
+function check() {
+  var tranid = document.getElementById("tranid").value;
+  if(tranid == '') {
+    return false;
+  }
+  return true;
+}
